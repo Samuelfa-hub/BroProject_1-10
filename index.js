1012b792231e14dd8dd60345175e3a2608b384d9
Samuelfa <samuelfaalmeida2008@gmail.com>
2025-04-17 23:40:00 -0300

    program finished

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-//creating the const to get button info 
-const 
-const
-const
-const 
+//creating the const to init the program
+const Init = document.getElementById("SubmtBtn");
+//Getting input and displayng info 
+Init.onclick = function() { 
+    let Height = prompt("Coloque sua Altura"); 
+    Height = Number(Height); 
+
+    let Weight = prompt("Coloque seu peso");
+    Weight = Number(Weight);
+
+    let IMC = Weight / (Height ** 2) *10000;
+
+    document.getElementById("IMCResults").textContent = "IMC: " + IMC ;
+
+    if(IMC <=18.5) {
+        document.getElementById("IMCCheck").textContent = "Parece que você está abaixo do peso"
+    }
+    else if (IMC <= 29.9 ){ 
+        document.getElementById("IMCCheck").textContent = "Parece que você está com sobrepeso"
+    }
+    else if ( IMC > 30){ 
+        document.getElementById("IMCCheck").textContent = "Parece que você está com obesidade"
+    }
+
+
+}
